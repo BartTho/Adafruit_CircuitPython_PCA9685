c47c7f2c99a79e4900dda428e15c4aa54834128a
Bart Thonissen <bart.thonissen@thomasmore.be>
2025-01-28 12:32:14 +0100

    MijnServo

--- examples/pca9685_simpletest.py
+++ examples/pca9685_simpletest.py
@@ -9,7 +9,7 @@ import board
 from adafruit_pca9685 import PCA9685
 
 # Create the I2C bus interface.
-i2c = board.I2C()  # uses board.SCL and board.SDA
+i2c = board.I2C())  # uses board.SCL and board.SDA
 # i2c = busio.I2C(board.GP1, board.GP0)    # Pi Pico RP2040
 
 # Create a simple PCA9685 class instance.
